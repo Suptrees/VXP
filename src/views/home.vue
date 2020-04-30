90d12c2c1171219b95e156e2a5f82b601c49667e
tflin <tflins@163.com>
2020-04-30 14:17:13 +0800

    fix: 修改 任务栏 组件

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <h1>主页</h1>
+  <div class="home">
+    <!-- <h1>主页</h1> -->
+    <task-bar></task-bar>
   </div>
 </template>
 
 <script>
-export default {}
+import TaskBar from '@/components/task-bar'
+
+export default {
+  components: {
+    TaskBar
+  }
+}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
